ccda31031b5de57b05ec1b1e8f9c08eb439b48ac
MrOutadi <mohammad6outadi@gmail.com>
2021-04-07 20:31:58 +0430

    components folder and pages files added to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+import Chats from './pages/chats'
+import Conversation from './pages/conversation'
+import Contacts from './pages/contacts'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/:username">
+            <Conversation />
+          </Route>
+          <Route path="/contacts">
+            <Contacts />
+          </Route>
+          <Route path="/">
+            <Chats />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/contacts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Contacts() {
+  return (
+    <div>
+      this is Contacts page
+    </div>
+  )
+}
