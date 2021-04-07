319de92ec5f4f34b4a6e926357e4048d4b5ee758
MrOutadi <mohammad6outadi@gmail.com>
2021-04-07 23:21:56 +0430

    moment and conversation date added to project

--- src/components/chatsItem/index.js
+++ src/components/chatsItem/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import {
-  Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
+import moment from 'moment';
 
 export default function ChatsItem(props) {
   const { conversation } = props;
+  const sent_at_time = moment(conversation.messages[0].sent_at);
   return (
     <Link to={`/${conversation.sent_by.username}`}>
       <div>{conversation.sent_by.first_name} {conversation.sent_by.last_name}</div>
       <div>{conversation.messages[0].content.substr(0, 100)}</div>
+      <div>{sent_at_time.hour()}:{sent_at_time.minute()}</div>
       <hr />
     </Link>
   )
