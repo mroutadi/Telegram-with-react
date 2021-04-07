676ddfe74482265546835906a6c4ada8f13c0725
MrOutadi <mohammad6outadi@gmail.com>
2021-04-07 23:50:37 +0430

    conversation structure added

--- src/pages/conversation.js
+++ src/pages/conversation.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 
+import ConversationData from '../mockData/conversations.json'
+
 export default function Conversation() {
   const url_parameters = useParams();
+  const conversation =
+    ConversationData.filter(conversation => conversation.sent_by.username === url_parameters.username)[0]
+
   return (
     <div>
-      this is Conversation page
+      <div>UserInfo</div>
+      <div>Conversations</div>
+      <div>
+        <textarea
+          name="messageInput"
+          placeholder="Write something ..."
+          value={conversation.draft_message}>
+
+        </textarea>
+        <button>
+          Send
+        </button>
+      </div>
     </div>
   )
 }
