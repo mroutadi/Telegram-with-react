967e1262fa819091205ae7b2c4893cbdc49e0fe5
MrOutadi <mohammad6outadi@gmail.com>
2021-04-09 01:26:01 +0430

    contacts sort added to project

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,12 +1,35 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { Context } from '../context';
 import ContactItem from '../components/contactItem'
 
 export default function Contacts() {
   const ctx = useContext(Context);
+  const [sortBy, setSortBy] = useState('NAME'); //NAME, LASTSEEN
+  const [contacts, setContacts] = useState(ctx.contacts.sort((a, b) => {
+    if (a.first_name > b.first_name) {
+      return 1;
+    } else if (a.first_name < b.first_name) {
+      return -1;
+    } else {
+      return a.last_name > b.last_name
+    }
+  }));
+  useEffect(() => {
+    if (sortBy === 'NAME') {
+      setContacts(contacts.sort((a, b) => {
+        if (a.first_name > b.first_name) {
+          return 1;
+        } else if (a.first_name < b.first_name) {
+          return -1;
+        } else {
+          return a.last_name > b.last_name
+        }
+      }))
+    }
+  }, [sortBy])
   return (
     <div>
-      {ctx.contacts.map(contact => (
+      {contacts.map(contact => (
         <ContactItem
           key={contact._id}
           contact={contact} />
