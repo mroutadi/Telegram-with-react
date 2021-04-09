a0c02b6c7dbc4b74f4418401479c23f76cb0f5fd
MrOutadi <mohammad6outadi@gmail.com>
2021-04-09 21:59:53 +0430

    user profile modal added

--- src/components/contactItem/index.js
+++ src/components/contactItem/index.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, { useState } from 'react'
 import LastSeen from '../../utils/lastSeen'
 import moment from 'moment';
 import styles from './styles/contactItem.module.scss'
+import UserModal from '../userProfile'
+import Modal from '@material-ui/core/Modal';
 
 export default function ContactItem(props) {
   const { contact } = props;
   const last_seen = moment(contact.last_seen);
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
   return (
-    <Link to={`/${contact.username}`} className={styles.contactItem}>
-      <div className={styles.imageContainer}>{
-        contact.picture[0] ?
-          <img src={contact.picture[0]} className={styles.itemImage} /> :
-          <span className={styles.itemNameImg} >{`${contact.first_name.charAt(0)}${contact.last_name.charAt(0)}`}</span>
-      }</div>
-      <div className={styles.nameAndLastseen}>
-        <div className={styles.userInfo}>{contact.first_name} {contact.last_name}</div>
-        <div className={styles.lastSeen}>{LastSeen(last_seen)}</div>
+    <React.Fragment>
+      <Modal
+        open={modalIsOpen}
+        onClose={() => setModalIsOpen(false)}
+      >
+        <UserModal contact={contact}></UserModal>
+      </Modal>
+      <div className={styles.contactItem} onClick={() => setModalIsOpen(true)}>
+        <div className={styles.imageContainer}>{
+          contact.picture[0] ?
+            <img src={contact.picture[0]} className={styles.itemImage} /> :
+            <span className={styles.itemNameImg} >{`${contact.first_name.charAt(0)}${contact.last_name.charAt(0)}`}</span>
+        }</div>
+        <div className={styles.nameAndLastseen}>
+          <div className={styles.userInfo}>{contact.first_name} {contact.last_name}</div>
+          <div className={styles.lastSeen}>{LastSeen(last_seen)}</div>
+        </div>
       </div>
-    </Link>
+    </React.Fragment >
   )
 }
---
+++ src/components/userProfile/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import styles from './styles/userProfile.module.scss'
+import LastSeen from '../../utils/lastSeen'
+import moment from 'moment';
+import ChatIcon from '@material-ui/icons/Chat';
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
+
+export default function UserModal(props) {
+  const { contact, me } = props
+  const last_seen = moment(contact.last_seen);
+  return (
+    <div className={styles.user}>
+      <div className={styles.userInformation}>
+        <div className={styles.imageContainer}>{
+          contact.picture[0] ?
+            <img src={contact.picture[0]} className={styles.itemImage} /> :
+            <span className={styles.itemNameImg} >{`${contact.first_name.charAt(0)}${contact.last_name.charAt(0)}`}</span>
+        }</div>
+        <div className={styles.nameAndLastseen}>
+          <div className={styles.userInfo}>{contact.first_name} {contact.last_name}</div>
+          <div className={styles.lastSeen}>{LastSeen(last_seen)}</div>
+        </div>
+        <div className={styles.userIcon}>{me ? <AddAPhotoIcon /> : <Link to={`/${contact.username}`}><ChatIcon /></Link>}</div>
+      </div>
+      <div className={styles.userAccount}>
+        <span className={styles.userAccountIcon}>Account</span>
+        {contact.phone && <div className={styles.userAccountItem}>
+          <h4 className={styles.userAccountItemExp}>{contact.phone}</h4>
+          <h6 className={styles.userAccountItemTitle}>Phone</h6>
+        </div>}
+        {contact.bio && <div className={styles.userAccountItem}>
+          <h4 className={styles.userAccountItemExp}>{contact.bio}</h4>
+          <h6 className={styles.userAccountItemTitle}>Bio</h6>
+        </div>}
+        {contact.username && <div className={styles.userAccountItem}>
+          <h4 className={styles.userAccountItemExp}>@{contact.username}</h4>
+          <h6 className={styles.userAccountItemTitle}>Username</h6>
+        </div>}
+      </div>
+    </div>
+  )
+}
