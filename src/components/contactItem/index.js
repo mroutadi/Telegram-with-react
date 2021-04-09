2689128b92ea32bccb154e373cea86446f0d0a61
MrOutadi <mohammad6outadi@gmail.com>
2021-04-09 19:20:46 +0430

    contacts page styles added

--- src/components/contactItem/index.js
+++ src/components/contactItem/index.js
@@ -2,15 +2,22 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import LastSeen from '../../utils/lastSeen'
 import moment from 'moment';
+import styles from './styles/contactItem.module.scss'
 
 export default function ContactItem(props) {
   const { contact } = props;
   const last_seen = moment(contact.last_seen);
   return (
-    <Link to={`/${contact.username}`}>
-      <div>{contact.first_name} {contact.last_name}</div>
-      <div>{LastSeen(last_seen)}</div>
-      <hr />
+    <Link to={`/${contact.username}`} className={styles.contactItem}>
+      <div className={styles.imageContainer}>{
+        contact.picture[0] ?
+          <img src={contact.picture[0]} className={styles.itemImage} /> :
+          <span className={styles.itemNameImg} >{`${contact.first_name.charAt(0)}${contact.last_name.charAt(0)}`}</span>
+      }</div>
+      <div className={styles.nameAndLastseen}>
+        <div className={styles.userInfo}>{contact.first_name} {contact.last_name}</div>
+        <div className={styles.lastSeen}>{LastSeen(last_seen)}</div>
+      </div>
     </Link>
   )
 }
