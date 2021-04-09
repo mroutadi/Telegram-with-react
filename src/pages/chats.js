7e586b73d0694a954e01268ceb79e998b1837a30
MrOutadi <mohammad6outadi@gmail.com>
2021-04-09 22:58:13 +0430

    new message icon improved, sort algorithms bugs checked

--- src/pages/chats.js
+++ src/pages/chats.js
@@ -4,20 +4,27 @@ import ChatsItem from '../components/chatsItem'
 import { Context } from '../context';
 import { Link } from 'react-router-dom'
 import styles from '../assets/styles/app.module.scss'
+import CreateIcon from '@material-ui/icons/Create';
 
 export default function Chats(props) {
   const ctx = useContext(Context);
-  const [conversations, setConversations] = useState(ctx && ctx.conversations.sort((a, b) =>
-    moment(a.messages[a.messages.length - 1].sent_at).isBefore(moment(b.messages[b.messages.length - 1].sent_at))
-  ))
+  const getConversation = () => {
+    if (ctx.conversations.lenght > 1) {
+      return ctx.conversations.sort((a, b) =>
+        moment(a.messages[a.messages.length - 1].sent_at).isBefore(moment(b.messages[b.messages.length - 1].sent_at)))
+    } else return ctx.conversations
+  }
+  const [conversations, setConversations] = useState(ctx && getConversation())
   return (
-    <div className={styles.chats} >
-      {conversations.map(conversation => (
-        <ChatsItem
-          key={conversation._id}
-          conversation={conversation} />
-      ))}
-      <Link to="/contacts" className={styles.newMessage}>Contacts</Link>
-    </div>
+    <React.Fragment>
+      <div className={styles.chats} >
+        {conversations.map(conversation => (
+          <ChatsItem
+            key={conversation._id}
+            conversation={conversation} />
+        ))}
+      </div>
+      <Link to="/contacts" className={styles.newMessage}><CreateIcon /></Link>
+    </React.Fragment>
   )
 }
--- src/utils/initialSort.js
+++ src/utils/initialSort.js
@@ -3,9 +3,11 @@ import moment from 'moment'
 export const ConversationsInitialSort = (conversations) => {  //sort by last message date
   let Conversations = conversations;
   for (let index = 0; index < Conversations.length; index++) {
-    Conversations[index].messages.sort((a, b) => {
-      return moment(a.sent_at).isAfter(moment(b.sent_at))
-    })
+    if (Conversations[index].messages.length > 1) {
+      Conversations[index].messages.sort((a, b) => {
+        return moment(a.sent_at).isAfter(moment(b.sent_at))
+      })
+    }
   }
   Conversations.sort((a, b) => {
     return moment(a.messages[a.messages.length - 1]).isAfter(moment(b.messages[b.messages.length - 1]))
