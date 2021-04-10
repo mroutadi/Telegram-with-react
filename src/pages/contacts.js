e2a428470d3244d963a3e411fa0a046baa0f7f4c
MrOutadi <mohammad6outadi@gmail.com>
2021-04-10 08:24:37 +0430

    bug fixed, contact header improved

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -1,10 +1,11 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { Context } from '../context';
 import ContactItem from '../components/contactItem'
+import styles from '../assets/styles/contacts.module.scss'
+import moment from 'moment'
 
 export default function Contacts() {
   const ctx = useContext(Context);
-  const [sortBy, setSortBy] = useState('NAME'); //NAME, LASTSEEN
   const [contacts, setContacts] = useState(ctx.contacts.sort((a, b) => {
     if (a.first_name > b.first_name) {
       return 1;
@@ -14,21 +15,13 @@ export default function Contacts() {
       return a.last_name > b.last_name
     }
   }));
-  useEffect(() => {
-    if (sortBy === 'NAME') {
-      setContacts(contacts.sort((a, b) => {
-        if (a.first_name > b.first_name) {
-          return 1;
-        } else if (a.first_name < b.first_name) {
-          return -1;
-        } else {
-          return a.last_name > b.last_name
-        }
-      }))
-    }
-  }, [sortBy, contacts])
+
+
   return (
-    <div>
+    <div className={styles.Contacts}>
+      <div className={styles.Header}>
+        <h3 className={styles.Title}>Contacts</h3>
+      </div>
       {contacts.map(contact => (
         <ContactItem
           key={contact._id}
